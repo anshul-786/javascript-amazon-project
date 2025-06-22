9eb03e51f6e2f65df57b0f6435d3de25662f089c
Anshul Sambher <sambher.official@gmail.com>
2025-06-23 01:01:13 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -2,13 +2,18 @@
 // keep all code related to same object in the same file
 // data is deduped using id - no need for other attribs
 
-export let cart = [{
-  productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-}, {
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || 
+  [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  }, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+  }];
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId, quantity) {
   let matchingItem;
@@ -27,6 +32,8 @@ export function addToCart(productId, quantity) {
       quantity
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -39,4 +46,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
