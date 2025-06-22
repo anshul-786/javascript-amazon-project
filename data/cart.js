c3e40366d0d79034748d6d27bd1996551b4a6172
Anshul Sambher <sambher.official@gmail.com>
2025-06-23 00:36:38 +0530

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -2,7 +2,7 @@
 // keep all code related to same object in the same file
 // data is deduped using id - no need for other attribs
 
-export const cart = [{
+export let cart = [{
   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 }, {
@@ -27,4 +27,16 @@ export function addToCart(productId, quantity) {
       quantity
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
