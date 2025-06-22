9b5a7bb80f9a587f1f068a27534f4003c2876957
Anshul Sambher <sambher.official@gmail.com>
2025-06-22 21:29:53 +0530

    Activated the quantity selector input

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach(
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -60,7 +60,8 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
+    const { productId } = button.dataset;
+    const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
     let matchingItem;
     let cartQuantity = 0;
 
@@ -71,11 +72,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity++;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId,
-        quantity: 1
+        quantity
       });
     }
 
